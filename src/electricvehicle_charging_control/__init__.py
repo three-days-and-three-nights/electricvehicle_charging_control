fca6ac44b7416f83bdcd9977e38d6185f67c6388
three-days-and-three-nights <1692565761@qq.com>
2024-09-07 12:27:37 +0800

    feat: First commit!

---
+++ src/electricvehicle_charging_control/__init__.py
@@ -0,0 +1,2 @@
+"""ElectricVehicle_Charging_Control"""
+__version__ = '0.1.0'
---
+++ tests/test_version.py
@@ -0,0 +1,7 @@
+"""Tests"""
+from electricvehicle_charging_control import __version__
+
+
+def test_version():
+    """Test version"""
+    assert __version__ == '0.1.0'
