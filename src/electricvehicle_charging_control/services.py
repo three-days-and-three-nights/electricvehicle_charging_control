f1f10595787c3d07b7d84f13670fcbcf9cf8b9b6
three-days-and-three-nights <1692565761@qq.com>
2024-09-09 16:33:05 +0800

    :sparkles: Add services to test.

---
+++ src/electricvehicle_charging_control/services.py
@@ -0,0 +1,22 @@
+from typing import Generic, List
+
+from sqlalchemy.orm import Session
+
+from electricvehicle_charging_control.dao import VehicleDAO, BaseDAO
+from electricvehicle_charging_control.models import Vehicle
+from electricvehicle_charging_control.schemas import CreateSchema, ModelType, UpdateSchema
+
+class BaseService(Generic[ModelType, CreateSchema, UpdateSchema]):
+    dao: BaseDAO
+
+    def get(self, session: Session, offset=0, limit=10) -> List[ModelType]:
+        """"""
+        return self.dao.get(session, offset=offset, limit=limit)
+
+    def get_by_id(self, session: Session, pk: int) -> ModelType:
+        """Get by id"""
+        return self.dao.get_by_id(session, pk)
+
+
+class VehicleService(BaseService[Vehicle, CreateSchema, UpdateSchema]):
+    dao = VehicleDAO()
